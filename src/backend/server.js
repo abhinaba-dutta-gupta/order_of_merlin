15889070c17bf5d2b3f94a5f695c335a0ecd02e6
Mrc-LM-10 <madhumitarc97@gmail.com>
2021-04-05 12:01:22 +0530

    back end connection

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+.connect('mongodb://127.0.0.1:27017/qustionnaire', {useNewUrlParser:true}, { useUnifiedTopology: true })
+.catch(e =>{
+    console.log('Connection Error', e.message)
+})
+
+const db = mongoose.connection;
+
+db.once('open', ()=>{
+    console.log("Mongo connected");
+})
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+
+
 app.listen(port, () => console.log(`Listening on port ${port}.`));
