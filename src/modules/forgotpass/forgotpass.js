c96b18c4c02922e47e016d5210498b257e36adc7
abhinaba-dutta-gupta <guild.saptam@gmail.com>
2021-04-08 14:58:29 +0530

    frontend validations

--- src/modules/forgotpass/forgotpass.js
+++ src/modules/forgotpass/forgotpass.js
@@ -4,26 +4,111 @@ import '../forgotpass/styles.css';
 import background from '../../images/background.jpg';
 import { IconButton } from '@material-ui/core';
 import Modal from '../../components/modal/modal';
-import { Mail, LockOpen, Security } from '@material-ui/icons';
+import { Mail, LockOpen, Security, Lock } from '@material-ui/icons';
 import { withRouter } from 'react-router-dom';
 import Appbar from '../../components/appbar/appbar';
 
 
 class ForgotPassword extends Component {
 
-    state = {
-        show: false,
-        show1: false
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fields: {},
+            errors: {},
+            show: false,
+        }
+    }
+
+    handleValidation() {
+        let fields = this.state.fields;
+        let errors = {};
+        let formIsValid = true;
+
+        //PersonalQuestion Validation
+        if (!fields["personalQuestion"]) {
+            formIsValid = false;
+            errors["personalQuestion"] = "Cannot be empty";
+        }
+
+        if (typeof fields["personalQuestion"] !== "undefined") {
+            if (!fields["personalQuestion"].match(/^[a-zA-Z]+$/)) {
+                formIsValid = false;
+                errors["personalQuestion"] = "Only letters";
+            }
+        }
+
+        //Email Validation
+        if (!fields["email"]) {
+            formIsValid = false;
+            errors["email"] = "Cannot be empty";
+        }
+
+        if (typeof fields["email"] !== "undefined") {
+            let lastAtPos = fields["email"].lastIndexOf('@');
+            let lastDotPos = fields["email"].lastIndexOf('.');
+
+            if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') === -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+                formIsValid = false;
+                errors["email"] = "Email is not valid";
+            }
+        }
+
+        this.setState({ errors: errors });
+        return formIsValid;
+    }
+
+    handlePasswordValidation() {
+        let fields = this.state.fields;
+        let errors = {};
+        let formIsValid = true;
+
+        //Password Validation
+        if (!fields["password"]) {
+            formIsValid = false;
+            errors["password"] = "Cannot be empty";
+        }
+
+        if (!(fields["password"].length >= 4 && fields["password"].length <= 8)) {
+            formIsValid = false;
+            errors["password"] = "Password should be between 4-8 characters length";
+        }
+
+        if(!(fields["password"] === fields["confirmPassword"])) {
+            formIsValid = false;
+            errors["password"] = "Passwords should be same";
+        }
+
+        this.setState({ errors: errors });
+        return formIsValid;
+    }
+
+    contactSubmit(e) {
+        e.preventDefault();
+
+        if (this.handleValidation()) {
+            this.showModal();
+        } 
+    }
+
+    contactPasswordSubmit(e) {
+        e.preventDefault();
+
+        if (this.handlePasswordValidation()) {
+            this.redirectToSignin();
+        } 
+    }
+
+    handleChange(field, e) {
+        let fields = this.state.fields;
+        fields[field] = e.target.value;
+        this.setState({ fields });
+    }
 
     showModal = e => {
         this.setState({
             show: !this.state.show
-        });
-    }
-    showModal1 = e => {
-        this.setState({
-            show1: !this.state.show1
         });
     }
 
@@ -41,32 +126,35 @@ class ForgotPassword extends Component {
                     <div className="card-head"><h1 className='card-title'><IconButton><LockOpen style={{ color: 'white' }} /></IconButton>Forgot Password</h1></div>
                     <br></br>
                     <br></br>
-                    <p><IconButton><Mail style={{ color: 'grey' }} /></IconButton><TextField required='required' type='e-mail' size='small' label="Email-ID" variant="standard" /></p>
-                    <button onClick={e => { this.showModal(e) }}>Submit</button>
+                    <form onSubmit={this.contactSubmit.bind(this)}>
+                        <p>
+                            <IconButton><Mail style={{ color: 'grey' }} /></IconButton>
+                            <TextField required='required' type='e-mail' size='small' label="Email-ID" variant="standard" onChange={this.handleChange.bind(this, "email")} />
+                        </p>
+                        <span style={{ color: "red" }}>{this.state.errors["email"]}</span>
+                        <p><IconButton><Security style={{ color: 'grey' }} /></IconButton>
+                            <TextField required='required' type='text' size='medium' label="Mother's maiden name" variant="standard" onChange={this.handleChange.bind(this, "personalQuestion")} />
+                        </p>
+                        <span style={{ color: "red" }}>{this.state.errors["personalQuestion"]}</span>
+                        <p><button type='submit'>Submit</button></p>
+                    </form>
                 </div>
-                <Modal onClose={this.showModal} show={this.state.show}>
-                    <p>Answer the security question to reset your password.</p>
-                    <p><IconButton>
-                        <Security style={{ color: 'grey' }} />
-                    </IconButton>
-                        <TextField required='required' type='personal-question' size='medium' label="Mother's maiden name" variant="standard" />
-                    </p>
-                    <button onClick={this.showModal1}>Submit</button>
-                </Modal>
 
-                <Modal onClose={this.showModal1} show={this.state.show1}>
+                <Modal onClose={this.showModal} show={this.state.show}>
                     <h1 className='title' > Reset Password </h1>
                     <br></br>
                     <div class="article-container">
                         <div class="article">
-                            <p><TextField id="filled-basic" required='required' type='password' size='small' label="New Password" variant="filled" /></p>
+                            <p><IconButton><Lock style={{ color: 'grey' }} /></IconButton>
+                               <TextField id="filled-basic" required='required' type='password' size='small' label="New Password" variant="standard" onChange={this.handleChange.bind(this, "password")} /></p>
                         </div>
                         <div class="article">
-                            <p><TextField id="filled-basic" required='required' type='password' size='small' label="Confirm New Password" variant="filled" /></p>
+                            <p><IconButton><Lock style={{ color: 'grey' }} /></IconButton>
+                               <TextField id="filled-basic" required='required' type='password' size='small' label="Confirm New Password" variant="standard" onChange={this.handleChange.bind(this, "confirmPassword")} /></p>
                         </div>
                     </div>
                     <br></br>
-                    <button onClick={this.redirectToSignin}>Submit</button>
+                    <button type='submit' onSubmit={this.contactPasswordSubmit.bind(this)} >Submit</button>
                 </Modal>
             </div>
         );
--- src/modules/login/login.js
+++ src/modules/login/login.js
@@ -9,6 +9,67 @@ import Appbar from '../../components/appbar/appbar';
 
 
 class Signin extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fields: {},
+            errors: {}
+        }
+    }
+
+    handleValidation() {
+        let fields = this.state.fields;
+        let errors = {};
+        let formIsValid = true;
+
+        //Password Validation
+        if (!fields["password"]) {
+            formIsValid = false;
+            errors["password"] = "Cannot be empty";
+        }
+
+        if (!(fields["password"].length>=4 && fields["password"].length<=8)) {
+            formIsValid = false;
+            errors["password"] = "Password should be between 4-8 characters length";
+        }
+
+        //Email Validation
+        if (!fields["email"]) {
+            formIsValid = false;
+            errors["email"] = "Cannot be empty";
+        }
+
+        if (typeof fields["email"] !== "undefined") {
+            let lastAtPos = fields["email"].lastIndexOf('@');
+            let lastDotPos = fields["email"].lastIndexOf('.');
+
+            if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') === -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+                formIsValid = false;
+                errors["email"] = "Email is not valid";
+            }
+        }
+
+        this.setState({ errors: errors });
+        return formIsValid;
+    }
+
+    contactSubmit(e) {
+        e.preventDefault();
+
+        if (this.handleValidation()) {
+            this.redirectToDashboard();
+        }
+
+    }
+
+    handleChange(field, e) {
+        let fields = this.state.fields;
+        fields[field] = e.target.value;
+        this.setState({ fields });
+    }
+
     redirectToDashboard = () => {
         const { history } = this.props;
         if (history) history.push('/dashboard');
@@ -21,10 +82,19 @@ class Signin extends Component {
                 <div className='paper-small'>
                     <div className="card-head"><h1 className='card-title'><IconButton><Lock style={{ color: 'white' }} /></IconButton>Login here</h1></div>
                     <br></br>
-                    <form onSubmit={this.redirectToDashboard}>
-                        <p><IconButton><Email style={{ color: 'grey' }} /></IconButton><TextField required='required' type='e-mail' size='small' label="Email-ID" variant="standard" /></p>
-                        <p><IconButton><Lock style={{ color: 'grey' }} /></IconButton><TextField required='required' type='password' size='small' label="Password" variant="standard" /></p>
-                        <button type='submit'>Submit</button>
+                    <form onSubmit={this.contactSubmit.bind(this)}>
+                        <p>
+                            <IconButton><Email style={{ color: 'grey' }} /></IconButton>
+                            <TextField required='required' type='e-mail' size='small' label="Email-ID" variant="standard" onChange={this.handleChange.bind(this, "email")} />
+                        </p>
+                        <span style={{ color: "red" }}>{this.state.errors["email"]}</span>
+                        <p>
+                            <IconButton><Lock style={{ color: 'grey' }} /></IconButton>
+                            <TextField required='required' type='password' size='small' label="Password" variant="standard" onChange={this.handleChange.bind(this, "password")} />
+                        </p>
+                        <span style={{ color: "red" }}>{this.state.errors["password"]}</span>
+                        <br></br>
+                        <p><button type='submit'>Submit</button></p>
                     </form>
                     <div style={{ alignContent: 'left' }}>
                         <p>Do not have an account?&nbsp;<Link to="./signup">REGISTER</Link></p>
